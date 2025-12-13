12062c4ae193098f4145ab8013f3f5c9f8a4fe96
Tabor Erickson <taborerickson1@gmail.com>
2025-12-13 11:32:23 -0600

    Completed Phase 1: project structure, Docker PostgreSQL, .env variables, DB configs

---
+++ config/db_connect.py
@@ -0,0 +1,22 @@
+import os 
+from dotenv import load_dotenv 
+from sqlalchemy import create_engine 
+
+load_dotenv() 
+
+def get_enging(): 
+    """  
+    - Centralized database access 
+    - Configuration driven by .env 
+    """
+    user = os.getenv("DB_USER") 
+    password = os.getenv("DB_PASSWORD") 
+    host = os.getenv("DB_HOST") 
+    port = os.getenv("DB_HOST") 
+    db = os.getenv("DB_NAME") 
+
+    url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    engine = create_engine(url)
+
+    return engine
+ 
